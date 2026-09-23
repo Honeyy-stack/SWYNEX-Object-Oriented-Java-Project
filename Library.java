75374953fc9fdcc57f04eba32c7ce86041c6df05
honeygoyal05 <honeygoyal0511@gmail.com>
2026-09-23 15:21:39 +0530

    Update Library.java

--- Library.java
+++ Library.java
@@ -1,1 +1,27 @@
+import java.util.*;
 
+class Library {
+    private ArrayList<Book> books = new ArrayList<>();
+
+    public void addBook(Book b) {
+        books.add(b);
+    }
+
+    public void showBooks() {
+        for (Book b : books) {
+            System.out.println(b.getId() + " - " + b.getTitle() +
+                    (b.isIssued() ? " (Issued)" : " (Available)"));
+        }
+    }
+
+    public void issueBook(int id) {
+        for (Book b : books) {
+            if (b.getId() == id && !b.isIssued()) {
+                b.issueBook();
+                System.out.println("Book issued!");
+                return;
+            }
+        }
+        System.out.println("Book not available");
+    }
+}
