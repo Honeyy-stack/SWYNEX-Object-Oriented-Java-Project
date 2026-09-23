fdf6387d36f2ebc3148978f36dd31eb55d2d59f7
honeygoyal05 <honeygoyal0511@gmail.com>
2026-09-23 15:22:22 +0530

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -1,1 +1,20 @@
+import java.util.*;
 
+public class MainApp {
+    public static void main(String[] args) {
+        Library lib = new Library();
+
+        lib.addBook(new Book(1, "Java Basics"));
+        lib.addBook(new Book(2, "OOP Concepts"));
+
+        Student s = new Student(101, "Honey");
+        Librarian l = new Librarian(201, "Admin");
+
+        s.displayInfo();
+        l.displayInfo();
+
+        lib.showBooks();
+        lib.issueBook(1);
+        lib.showBooks();
+    }
+}
