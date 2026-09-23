f329f3afcabe7b48fda277064483c93c6f7a7506
honeygoyal05 <honeygoyal0511@gmail.com>
2026-09-23 15:20:59 +0530

    Update Librarian.java

--- Librarian.java
+++ Librarian.java
@@ -1,1 +1,11 @@
+class Librarian extends User {
 
+    public Librarian(int id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    void displayInfo() {
+        System.out.println("Librarian ID: " + id + ", Name: " + name);
+    }
+}
