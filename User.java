21cef5d58662fa3f65aa37c336114f4431d340dc
honeygoyal05 <honeygoyal0511@gmail.com>
2026-09-23 15:23:23 +0530

    Define abstract User class with properties and constructor

--- User.java
+++ User.java
@@ -1,1 +1,11 @@
+abstract class User {
+    protected int id;
+    protected String name;
 
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    abstract void displayInfo();
+}
