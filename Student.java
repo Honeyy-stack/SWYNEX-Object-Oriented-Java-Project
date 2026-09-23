80d0de32034639346c4d40f87289d9b2292102c9
honeygoyal05 <honeygoyal0511@gmail.com>
2026-09-23 15:22:58 +0530

    Add Student class extending User class

--- Student.java
+++ Student.java
@@ -1,1 +1,11 @@
+class Student extends User {
 
+    public Student(int id, String name) {
+        super(id, name);
+    }
+
+    @Override
+    void displayInfo() {
+        System.out.println("Student ID: " + id + ", Name: " + name);
+    }
+}
